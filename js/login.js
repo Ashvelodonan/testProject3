4d6e346d3a6a6715b5cebf2c20b7e312fb1ddd52
Ashvelodonan <125660609+Ashvelodonan@users.noreply.github.com>
2024-10-04 01:25:19 +0400

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -5,7 +5,7 @@ var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
 if ( username == "test3" && password == "test3#123"){
 alert ("Login successfully");
-window.location = "index2.html"; // Redirecting to other page.
+window.location = "livestick.html"; // Redirecting to other page.
 return false;
 }
 else{
