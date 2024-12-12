966fc26de9eec1776c0d2d6f88c5149de0e795b4
Ashvelodonan <125660609+Ashvelodonan@users.noreply.github.com>
2024-12-13 02:00:37 +0400

    Update login.js

--- js/login.js
+++ js/login.js
@@ -3,7 +3,7 @@ var attempt = 3; // Variable to count number of attempts.
 function validate(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
-if ( username == "test3" && password == "test3#123"){
+if ( username == "test3" && password == "babanam3#123"){
 alert ("Login successfully");
 window.location = "livestick.html"; // Redirecting to other page.
 return false;
